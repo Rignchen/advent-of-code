66f131650f5e6ca91daa8b2461790d3809cd2eef
Rignchen <rinchendemort@gmail.com>
2024-05-17 21:49:56 +0200

    1.1 multiply numbers

--- 01_F#/src/Program.fs
+++ 01_F#/src/Program.fs
@@ -1,5 +1,6 @@
 let readLines filePath = System.IO.File.ReadLines(filePath);;
 let inline add a b = a + b;;
+let inline multiply a b = a * b;;
 
 let lines = readLines "example.txt";;
 let numbers = lines |> Seq.map int |> Seq.toList;;
@@ -8,4 +9,6 @@ for i in 0..numbers.Length-2 do
     for j in i+1..numbers.Length-1 do
         let s = add numbers.[i] numbers.[j]
         if s = 2020 then
-            printfn "%d + %d = %d" numbers.[i] numbers.[j] s;;
+            let p = multiply numbers.[i] numbers.[j]
+            printfn "%d + %d = %d" numbers.[i] numbers.[j] s
+            printfn "%d * %d = %d" numbers.[i] numbers.[j] p;;
