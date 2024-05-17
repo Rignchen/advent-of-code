5e14da29517591d0bab5647b42eabe4c5bb9e7b1
Rignchen <rinchendemort@gmail.com>
2024-05-17 21:20:58 +0200

    1.1: iterate over numbers

--- 01_F#/src/Program.fs
+++ 01_F#/src/Program.fs
@@ -2,5 +2,6 @@ let readLines filePath = System.IO.File.ReadLines(filePath);;
 let lines = readLines "example.txt";;
 let numbers = lines |> Seq.map int |> Seq.toList;;
 
-printfn "%A" numbers;;
 
+for i in 0..numbers.Length-1 do
+   printfn "%d"%d" numbers[i]
