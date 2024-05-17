e56310336a41a6fc317619871118524934b06d6d
Rignchen <rinchendemort@gmail.com>
2024-05-17 22:00:14 +0200

    1.2: expand to 3 nested loops

--- 01_F#/src/Program.fs
+++ 01_F#/src/Program.fs
@@ -1,15 +1,16 @@
 let readLines filePath = System.IO.File.ReadLines(filePath);;
-let inline add a b = a + b;;
-let inline multiply a b = a * b;;
+let inline add a b c = a + b + c;;
+let inline multiply a b c = a * b * c;;
 
-let lines = readLines "input.txt";;
+let lines = readLines "example.txt";;
 let numbers = lines |> Seq.map int |> Seq.toList;;
 
-for i in 0..numbers.Length-2 do
-    for j in i+1..numbers.Length-1 do
-        let s = add numbers.[i] numbers.[j]
-        if s = 2020 then
-            let p = multiply numbers.[i] numbers.[j]
-            printfn "%d + %d = %d" numbers.[i] numbers.[j] s
-            printfn "%d * %d = %d" numbers.[i] numbers.[j] p;;
+for i in 0..numbers.Length-3 do
+    for j in i+1..numbers.Length-2 do
+        for k in j+1..numbers.Length-1 do
+            let s = add numbers.[i] numbers.[j] numbers.[k]
+            if s = 2020 then
+                let p = multiply numbers.[i] numbers.[j] numbers.[k]
+                printfn "%d + %d + %d = %d" numbers.[i] numbers.[j] numbers.[k] s
+                printfn "%d * %d * %d = %d" numbers.[i] numbers.[j] numbers.[k] p;;
 
