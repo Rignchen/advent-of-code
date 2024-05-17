0201a0d5d6c8f64c7d399b12e631ef61f5bf6622
Rignchen <rinchendemort@gmail.com>
2024-05-17 21:25:29 +0200

    1.1 nested loop, only 1 time each combinaison

--- 01_F#/src/Program.fs
+++ 01_F#/src/Program.fs
@@ -2,6 +2,6 @@ let readLines filePath = System.IO.File.ReadLines(filePath);;
 let lines = readLines "example.txt";;
 let numbers = lines |> Seq.map int |> Seq.toList;;
 
-
-for i in 0..numbers.Length-1 do
-   printfn "%d"%d" numbers[i]
+for i in 0..numbers.Length-2 do
+    for j in i+1..numbers.Length-1 do
+        printfn "%d %d" numbers.[i] numbers.[j];;
