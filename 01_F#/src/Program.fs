2dcb98206ed8b3b84c6fedbc5bbad269866d61a3
Rignchen <rinchendemort@gmail.com>
2024-05-17 21:46:07 +0200

    1.1 addition int

--- 01_F#/src/Program.fs
+++ 01_F#/src/Program.fs
@@ -1,7 +1,10 @@
 let readLines filePath = System.IO.File.ReadLines(filePath);;
+let inline add a b = a + b;;
+
 let lines = readLines "example.txt";;
 let numbers = lines |> Seq.map int |> Seq.toList;;
 
 for i in 0..numbers.Length-2 do
     for j in i+1..numbers.Length-1 do
-        printfn "%d %d" numbers.[i] numbers.[j];;
+        let s = add numbers.[i] numbers.[j]
+        printfn "%d + %d = %d" numbers.[i] numbers.[j] s;;
