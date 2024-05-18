702e010e68b3f3cb4f8d581e6d700cf759eb20ce
Rignchen <rinchendemort@gmail.com>
2024-05-18 12:59:06 +0200

    2.1: read all lines

--- 02_D/src/script_1.d
+++ 02_D/src/script_1.d
@@ -3,6 +3,9 @@ import std.stdio;
 void main(string[] args)
 {
         auto file = File(args[1]);
-        writefln("First line: %s", file.readln());
+        while(!file.eof())
+        {
+                writefln("Line: %s", file.readln());
+        }
         file.close();
 }
