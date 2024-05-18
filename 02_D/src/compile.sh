ed19b973d457471dbe79a5790c6a547de9c88ae6
Rignchen <rinchendemort@gmail.com>
2024-05-18 12:26:30 +0200

    2.1: move executables to bin directory

--- 02_D/src/compile.sh
+++ 02_D/src/compile.sh
@@ -1,7 +1,7 @@
-# get the list of all files ending with .d and compile them with ``gdc <name>.d -o <name>.o``
+# get the list of all files ending with .d and compile them with ``gdc <name>.d -o bin/<name>.o``
 FILES=$(ls *.d)
 for file in $FILES
 do
-    gdc $file -o ${file%.d}.o
+    gdc $file -o bin/${file%.d}.o
 done
 
