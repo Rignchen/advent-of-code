73112353185b7e649c55c6a13feeb0f889c1858f
Rignchen <rinchendemort@gmail.com>
2024-05-18 12:53:11 +0200

    2.1: read 1st line of file

--- 02_D/src/script_1.d
+++ 02_D/src/script_1.d
@@ -3,8 +3,6 @@ import std.stdio;
 void main(string[] args)
 {
         auto file = File(args[1]);
-        writefln("File size: %s", file.size());
-        writefln("File name: %s", file.name());
-        writefln("File type: %s", typeid(file));
+        writefln("First line: %s", file.readln());
         file.close();
 }
