fd2c719709ce1d998b196b873df7e98e3d2279de
Rignchen <rinchendemort@gmail.com>
2024-05-18 17:49:40 +0200

    3.1: count the #

--- 03_Julia/src/script_1.jl
+++ 03_Julia/src/script_1.jl
@@ -8,10 +8,15 @@ end
 println(text)
 
 y = 0
+count = 0
 for line in text
         println("$line : $y")
         if line[y % length(line)+1] == '#'
                 println("$y: $line")
+                global count += 1
         end
         global y += 3
 end
+
+println("Count of tree: $count")
+
