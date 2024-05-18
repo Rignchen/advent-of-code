a2cf2b911efc22deb4401fc5e383feae3887dbb4
Rignchen <rinchendemort@gmail.com>
2024-05-18 13:06:32 +0200

    2.1: get all lines in an array

--- 02_D/src/script_1.d
+++ 02_D/src/script_1.d
@@ -3,9 +3,13 @@ import std.stdio;
 void main(string[] args)
 {
         auto file = File(args[1]);
+        string[] text;
         while(!file.eof())
         {
-                writefln("Line: %s", file.readln());
+                string line = file.readln();
+                writef("Line: %s", line);
+                text ~= line;
         }
         file.close();
+        writefln("Text: %s", text);
 }
