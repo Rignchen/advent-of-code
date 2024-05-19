30171d558bfba82013d432b03ec848ac1302de4f
Rignchen <rinchendemort@gmail.com>
2024-05-19 02:52:14 +0200

    3.2: handle multiple jump size

--- 03_Julia/src/script_2.jl
+++ 03_Julia/src/script_2.jl
@@ -7,16 +7,22 @@ open("/app/example.txt","r") do f
 end
 println(text)
 
-y = 0
-count = 0
-for line in text
-        println("$line : $y")
-        if line[y % length(line)+1] == '#'
-                println("$y: $line")
-                global count += 1
+dif_x = [1,3,5,7,1]
+dif_y = [1,1,1,1,2]
+for i in 1:5
+        x = 0
+        y = 0
+        count = 0
+        while y < length(text)
+                line = text[y + 1]
+                println("$line : $x - $y")
+                if line[x % length(line) + 1] == '#'
+                        println("$x - $y: $line")
+                        count += 1
+                end
+                x += dif_x[i]
+                y += dif_y[i]
         end
-        global y += 3
+        println("Count of tree for slide $i: $count")
 end
-
-println("Count of tree: $count")
 
