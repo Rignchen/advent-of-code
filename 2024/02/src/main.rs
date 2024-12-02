9e64b5076da8caa60fb588af84dbf2527bc4490d
Rignchen <rinchendemort@gmail.com>
2024-12-02 09:30:59 +0100

    feat(2024): solve day 2 puzzle 2

--- 2024/02/src/main.rs
+++ 2024/02/src/main.rs
@@ -3,6 +3,7 @@ struct Report {
 	entries: Vec<i8>,
 	is_safe: bool,
 	is_increasing: bool,
+	removed: i8,
 }
 
 impl Report {
@@ -10,16 +11,22 @@ impl Report {
 		// entrie is a string of numbers separated by a space
 		let entries: Vec<i8> = entrie.split_whitespace().map(|n| n.parse().unwrap()).collect();
 
-		// set the is_increasing field if all numbers are increasing/decresing
-		// set the is_safe field to false if some number are increasing and some are decreasing
-		// the difference between each number must be at least 1 and at most 2
-		let mut is_increasing = entries.iter().zip(entries.iter().skip(1)).all(|(a, b)| a < b);
-		let mut is_safe       = entries.iter().zip(entries.iter().skip(1)).all(|(a, b)| is_increasing == (a < b) && 1 <= (b - a).abs() && (b - a).abs() <= 3);
+		let (is_increasing, is_safe, removed) = run_with_list_but_one(entries.clone(), |entries| {
+			let zip: Vec<(&i8, &i8)> = entries.iter().zip(entries.iter().skip(1)).collect();
+
+			// safe if all values are increasing or decreasing and the max difference between two
+			// consecutive values is 3
+			let is_increasing = zip.iter().all(|(&a, &b)| a < b);
+			let is_safe = zip.iter().all(|(&a, &b)| is_increasing == (a < b) && (b - a).abs() <= 3 && b != a);
+
+			(is_increasing, is_safe)
+		});
 
 		Report {
 			entries: entries,
 			is_safe: is_safe,
 			is_increasing: is_increasing,
+			removed: removed,
 		}
 	}
 }
@@ -27,6 +34,7 @@ impl Report {
 fn main() {
 	let input = get_input();
 	// how many reports are safe
+	input.iter().for_each(|r| println!("{:?}", r));
 	println!("{}", input.iter().filter(|r| r.is_safe).count());
 }
 
@@ -36,3 +44,19 @@ fn get_input() -> Vec<Report> {
 	contents.lines().map(|l| Report::new(l.to_string())).collect()
 }
 
+fn run_with_list_but_one<T>(v: Vec<T>, f: fn(Vec<T>) -> (bool, bool)) -> (bool, bool, i8) where T: Clone {
+	let mut res;
+	res = f(v.clone());
+	if res.1 {
+		return (res.0, res.1, -1);
+	}
+	for i in 0..v.len() {
+		let mut v2 = v.clone();
+		v2.remove(i);
+		res = f(v2);
+		if res.1 {
+			return (res.0, res.1, i as i8);
+		}
+	}
+	(false, false, -1)
+}
