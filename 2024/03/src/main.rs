5ad175a8016d2ce22d11bdaf4a98b4e1c5de07dc
Rignchen <rinchendemort@gmail.com>
2024-12-03 07:24:47 +0100

    feat(2024): solve day 3 puzzle 2

--- 2024/03/src/main.rs
+++ 2024/03/src/main.rs
@@ -1,17 +1,31 @@
 fn main() {
 	let input = get_input();
 	// match a regex to get the tokens
-	let re = regex::Regex::new(r"mul\(\d+,\d+\)").unwrap();
+	let re = regex::Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").unwrap();
 	let mut sum = 0;
+	let mut is_actif = true;
 	for cap in re.captures_iter(&input) {
 		let mut nums = cap.get(0).unwrap().as_str();
-		// remove "mul(" and ")"
-		println!("{}", nums);
-		nums = &nums[4..nums.len()-1];
-		let mut iter = nums.split(",");
-		let a: i32 = iter.next().unwrap().parse().unwrap();
-		let b: i32 = iter.next().unwrap().parse().unwrap();
-		sum += a * b;
+		match nums {
+			"do()" => {
+				is_actif = true;
+			},
+			"don't()" => {
+				is_actif = false;
+			},
+			_ => {
+				if !is_actif {
+					continue;
+				}
+				// remove "mul(" and ")"
+				println!("{}", nums);
+				nums = &nums[4..nums.len()-1];
+				let mut iter = nums.split(",");
+				let a: i32 = iter.next().unwrap().parse().unwrap();
+				let b: i32 = iter.next().unwrap().parse().unwrap();
+				sum += a * b;
+			}
+		}
 	}
 	println!("Sum: {}", sum);
 }
