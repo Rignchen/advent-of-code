7fec16c35ca43c4a8f4b512027f422080b764b65
Rignchen <rinchendemort@gmail.com>
2024-12-08 19:21:10 +0100

    feat(2020): resolve day 9 problem 2

--- 2020-1day1language/09_Go/src/script1.go
+++ 2020-1day1language/09_Go/src/script1.go
@@ -6,22 +6,41 @@ import (
 	"os"
 	"strings"
 	"strconv"
+	"errors"
 )
 
 func main() {
 	input := get_input()
+	copy_input := make([]int, len(input))
+	copy(copy_input, input)
 	preamble_size, err := strconv.Atoi(os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
 	preamble := get_preamble(&input, preamble_size)
+	weakness := 0
 	for _, value := range input {
 		if !is_sum_of_two(&preamble, value) {
-			fmt.Println(value)
+			weakness = value
 			break
 		}
 		preamble = append(preamble[1:], value)
 	}
+	sum_range, err := find_sum_range(&copy_input, weakness)
+	if err != nil {
+		log.Fatal(err)
+	}
+	min := sum_range[0]
+	max := sum_range[0]
+	for _, value := range sum_range {
+		if value < min {
+			min = value
+		}
+		if value > max {
+			max = value
+		}
+	}
+	fmt.Println(min + max)
 }
 
 func get_input() []int {
@@ -62,4 +81,25 @@ func is_sum_of_two(preamble *[]int, value int) bool {
 		}
 	}
 	return false
+}
+
+func sum_from_to(input *[]int, from int, to int) int {
+	// sum the elements from index from to index to in the input
+	sum := 0
+	for _, value := range (*input)[from:to] {
+		sum += value
+	}
+	return sum
+}
+
+func find_sum_range(input *[]int, value int) ([]int, error) {
+	// find a contiguous set of at least two numbers in the input that sum to value
+	for i := 0; i < len(*input); i++ {
+		for j := i + 1; j < len(*input); j++ {
+			if sum_from_to(input, i, j) == value {
+				return (*input)[i:j], nil
+			}
+		}
+	}
+	return nil, errors.New("No contiguous set found")
 }
