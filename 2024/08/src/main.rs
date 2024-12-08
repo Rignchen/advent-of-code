36428f8b1345c04ef9f408d56384bbcb350f547c
Rignchen <rinchendemort@gmail.com>
2024-12-08 11:49:25 +0100

    feat(2024): resolve day 8 problem 1

--- 2024/08/src/main.rs
+++ 2024/08/src/main.rs
@@ -1,5 +1,7 @@
-fn get_input() -> (Vec<Antena>, (i32, i32)) {
-	let file = "data/example.txt";
+use std::collections::HashMap;
+
+fn get_input() -> (Vec<(char, Position)>, (i32, i32)) {
+	let file = "data/input.txt";
 	let contents = std::fs::read_to_string(file).unwrap();
 	let mut lines = contents.lines();
 	let map_size = (lines.clone().count() as i32, lines.next().unwrap().len() as i32);
@@ -7,11 +9,7 @@ fn get_input() -> (Vec<Antena>, (i32, i32)) {
 	for (y, line) in contents.lines().enumerate() {
 		for (x, c) in line.chars().enumerate() {
 			if c != '.' {
-				antenas.push(Antena {
-					signal: c,
-					x: x as i32,
-					y: y as i32,
-				});
+				antenas.push((c, Position { x: x as i32, y: y as i32 }));
 			}
 		}
 	}
@@ -19,13 +17,56 @@ fn get_input() -> (Vec<Antena>, (i32, i32)) {
 }
 
 fn main() {
-	let input = get_input();
-	println!("{:?}", input);
+	let (antenas, map_size) = get_input();
+	let mut antena_map = HashMap::<char, Vec<Position>>::new();
+	for antena in antenas {
+		antena_map.entry(antena.0).or_insert(Vec::new()).push(antena.1);
+	}
+	let mut antinode = Vec::<Position>::new();
+
+	for positions in antena_map.values() {
+		for position in positions {
+			for other_position in positions {
+				if position == other_position {
+					continue;
+				}
+				let vector = position.vector(other_position);
+				let to_push = position.from_vector(vector);
+				if !antinode.contains(&to_push) && to_push.in_map(map_size) {
+					antinode.push(to_push);
+				}
+				let to_push = other_position.from_vector_reverse(vector);
+				if !antinode.contains(&to_push) && to_push.in_map(map_size) {
+					antinode.push(to_push);
+				}
+			}
+		}
+	}
+	println!("{:?}", antinode.len());
 }
 
-#[derive(Debug)]
-struct Antena {
-	signal: char,
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Position {
 	x: i32,
 	y: i32,
+}
+impl Position {
+	fn vector(&self, other: &Position) -> (i32, i32) {
+		((self.x - other.x), (self.y - other.y))
+	}
+	fn from_vector(&self, vector: (i32, i32)) -> Position {
+		Position {
+			x: self.x + vector.0,
+			y: self.y + vector.1,
+		}
+	}
+	fn from_vector_reverse(&self, vector: (i32, i32)) -> Position {
+		Position {
+			x: self.x - vector.0,
+			y: self.y - vector.1,
+		}
+	}
+	fn in_map(&self, map_size: (i32, i32)) -> bool {
+		self.x >= 0 && self.y >= 0 && self.x < map_size.0 && self.y < map_size.1
+	}
 }
