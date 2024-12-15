c6929bc3c4c03878e276e85e813c31cb037e4560
Rignchen <rinchendemort@gmail.com>
2024-12-15 19:00:54 +0100

    feat(2024): make robots move

--- 2024/14/src/main.rs
+++ 2024/14/src/main.rs
@@ -10,7 +10,23 @@ fn get_input() -> (Vec<Robot>, (i32, i32)) {
 
 fn main() {
 	let (mut robots, map_size) = get_input();
-   println!("{:?}", robots);
+	// after 100 seconds
+	robots.iter_mut().for_each(|robot| robot.move_robot(100, map_size));
+	print_map(&robots, map_size);
+}
+
+fn print_map(robots: &Vec<Robot>, map_size: (i32, i32)) {
+	let mut map = vec![vec![0; map_size.0 as usize]; map_size.1 as usize];
+	robots.iter()
+		.for_each(|robot| {
+		map[robot.position.1 as usize][robot.position.0 as usize] += 1;
+	});
+	map.iter().for_each(|row| {
+		row.iter().for_each(|&cell| {
+			print!("{}", if cell == 0 { '.' } else { cell.to_string().chars().next().unwrap() });
+		});
+		println!();
+	});
 }
 
 #[derive(Debug)]
@@ -31,6 +47,21 @@ impl Robot {
 				caps[3].parse().unwrap(),
 				caps[4].parse().unwrap(),
 			),
+		}
+	}
+
+	fn move_robot(&mut self, seconds: i32, map_size: (i32, i32)) {
+		self.position.0 += self.velocity.0 * seconds;
+		self.position.1 += self.velocity.1 * seconds;
+
+		self.position.0 %= map_size.0;
+		self.position.1 %= map_size.1;
+
+		if self.position.0 < 0 {
+			self.position.0 += map_size.0;
+		}
+		if self.position.1 < 0 {
+			self.position.1 += map_size.1;
 		}
 	}
 }
