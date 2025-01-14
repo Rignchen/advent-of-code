d4b12320ede56d0a26f8b79a009efb315a9f37d3
Rignchen <rinchendemort@gmail.com>
2025-01-14 12:16:16 +0100

    feat(2020): get adjacent seats

--- 2020-1day1language/11_Javascript/src/script1.js
+++ 2020-1day1language/11_Javascript/src/script1.js
@@ -6,4 +6,28 @@ if (process.argv.length == 2) {
 }
 let content = fs.readFileSync(process.argv[2], 'utf8').split('\n');
 
+/**
+ * Return all adjacent entry to a given entry
+ * @param {number} x
+ * @param {number} y
+ * @param {any[]} content
+ * @returns {any[]}
+ */
+function adjacent(x, y, content) {
+	let adj = [];
+	for (let i = x - 1; i <= x + 1; i++) {
+		for (let j = y - 1; j <= y + 1; j++) {
+			if (i < 0 || j < 0 || i >= content.length || j >= content[i].length) continue;
+			if (i == x && j == y) continue;
+			adj.push(content[i][j]);
+		}
+	}
+	return adj;
+}
+
+let old;
+while (content !== old) {
+	old = content;
+}
+
 console.log(content);
